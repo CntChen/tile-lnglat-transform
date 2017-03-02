4ef096321a99c439b67fae20648d34df97bbe4c3
CUNTIE <cntchen@gmail.com>
2017-03-03 02:07:26 +0800

    add webpack uglify

--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,17 @@ var CleanPlugin = require('clean-webpack-plugin');
 
 var plugins = [
   new CleanPlugin('builds'),
-  // new webpack.optimize.CommonsChunkPlugin({
-  //   name: 'main',
-  //   children: true,
-  //   minChunks: 1,
-  // }),
-  //   new webpack.optimize.UglifyJsPlugin({
-  //     compress: {
-  //         warnings: false
-  //     },
-  // })
 ];
 
-module.exports = {
+var pluginsWithMin = plugins.concat([
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+  })
+]);
+
+module.exports = [{
   entry: './src',
   output: {
     path: 'builds/',
@@ -33,4 +31,21 @@ module.exports = {
       loader: 'babel-loader',
     }, ],
   }
-};
+},
+{
+  entry: './src',
+  output: {
+    path: 'builds/',
+    filename: 'index.min.js',
+    libraryTarget: "umd",
+    library: 'TileLnglatTransform',
+  },
+  plugins: pluginsWithMin,
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      include: /src/,
+      loader: 'babel-loader',
+    }, ],
+  }
+}];
